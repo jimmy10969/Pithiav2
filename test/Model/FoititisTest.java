c16799a45f91240c8c57dbb6565b849cd1ce7797
Jimmy M <57236583+jimmy10969@users.noreply.github.com>
2019-12-08 19:18:11 +0200

    1

---
+++ test/Model/FoititisTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jimmy
+ */
+public class FoititisTest {
+    
+    public FoititisTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    /**
+     * Test of addDhlwsh method, of class Foititis.
+     */
+    @Test
+    public void testAddDhlwsh() {
+        System.out.println("addDhlwsh");
+        Mathima mathima = null;
+        String hmeromDillwsis = "";
+        Foititis instance = new Foititis();
+        instance.addDhlwsh(mathima, hmeromDillwsis);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addVathmos method, of class Foititis.
+     */
+    @Test
+    public void testAddVathmos() {
+        System.out.println("addVathmos");
+        Mathima mathima = null;
+        String hmeromExetasis = "";
+        double vathmos = 0.0;
+        Foititis instance = new Foititis();
+        instance.addVathmos(mathima, hmeromExetasis, vathmos);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    
+    
+}
